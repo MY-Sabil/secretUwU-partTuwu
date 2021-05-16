9213fa7b9e5757077a5e70d0be2f2133c86f6a32
MY-Sabil <55656930+MY-Sabil@users.noreply.github.com>
2021-05-16 12:06:56 +0600

    Create thingy.js

---
+++ thingy.js
@@ -0,0 +1,9 @@
+console.log("%cLast One!! \n" , "font-size: 7rem; text-align: center; color: #fcedd8; font-family: 'Niconne', cursive;  font-weight: 700; text-shadow: 5px 5px 0px #eb452b, 10px 10px 0px #efa032, 15px 15px 0px #46b59b, 20px 20px 0px #017e7f,25px 25px 0px #052939,30px 30px 0px #c11a2b,35px 35px 0px #c11a2b,40px 40px 0px #c11a2b,45px 45px 0px #c11a2b;")
+
+console.log("%cSo this is the easiest one yet" , "color: #017e7f; font-family: 'Abel', sans-serif; font-size: 20px;")
+console.log("%cCause I am too lazy to figure out some new puzzles :v" , "color: #017e7f; font-family: 'Abel', sans-serif; font-size: 20px;")
+console.log("%cJust solve the bottom one to finish this game" , "color: #017e7f; font-family: 'Abel', sans-serif; font-size: 20px;")
+console.log("%cThen just inbox me in discord at NinjaByte#9397" , "color: #017e7f; font-family: 'Abel', sans-serif; font-size: 20px;")
+console.log("%cThe first person who'll inbox me the result first wins!!" , "color: #017e7f; font-family: 'Abel', sans-serif; font-size: 20px;")
+console.log(" ")
+console.log("%cPh sklqlvkhg wkh wklqjb!! qrz jle ph prqhb ru hovh!! dqg Kl Qlqmd XzX" , "color: #eb452b; font-family: 'Abel', sans-serif; font-size: 19px;")
